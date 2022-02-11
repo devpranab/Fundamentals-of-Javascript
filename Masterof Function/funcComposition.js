3d901fd22a3f43bf60b66f16b3abd68df320e6f5
Pranab Sarkar <86111675+devpranab@users.noreply.github.com>
2022-02-11 07:59:50 +0530

    Update funcComposition.js

--- Masterof Function/funcComposition.js
+++ Masterof Function/funcComposition.js
@@ -1,1 +1,22 @@
 //function composition
+//function composition - it's a shortcut way pattern
+//as a input of first function can give output of second function
+
+function print(inp){
+    console.log(inp);
+}
+
+function multiple(n){
+    return n*5
+}
+
+function add(a,b){
+    return a+b
+}
+
+print((multiple(add(3,5)))); 
+
+/* 3+5 = 8 return a+b, working down to up
+   n * 5 return n*5
+   8 console.log(inp);
+*/ 
