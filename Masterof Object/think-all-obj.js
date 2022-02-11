63623093386856248a2d8794315375a78f614765
Pranab Sarkar <86111675+devpranab@users.noreply.github.com>
2022-02-11 11:06:53 +0530

    Update think-all-obj.js
    update think everything can be an object

--- Masterof Object/think-all-obj.js
+++ Masterof Object/think-all-obj.js
@@ -1,2 +1,8 @@
 //Think everything is a object
 //Everything which takes multiple properties to describe is an object
+
+//for describe to anything then need multiple data
+
+//in js, number is primitive, but we can make object to number
+//ex-
+let n = 23.45 //here 2part one integer other one floating
