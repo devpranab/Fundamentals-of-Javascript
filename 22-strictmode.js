35e2d33ae4cc10821f370646201b98c1d18848f2
Pranab Sarkar <86111675+devpranab@users.noreply.github.com>
2022-02-11 12:39:47 +0530

    Update 22-strictmode.js
    //strict mode in Javascript

--- 22-strictmode.js
+++ 22-strictmode.js
@@ -1,1 +1,62 @@
 //strict mode in Javascript
+//js find simple mistake
+
+// "use strict";
+// x = 3.14;       // This will cause an error because x is not declared
+
+// "use strict";
+// myFunction();
+
+// function myFunction() {
+//   y = 3.14;   // This will also cause an error because y is not declared
+// }
+
+//but without strict mode can't show error, it will work.
+
+//can i use particular scope
+// x = 3.14;       // This will not cause an error.
+// myFunction();
+
+// function myFunction() {
+//   "use strict";
+//   y = 3.14;   // This will cause an error
+// }
+
+//Strict mode changes previously accepted "bad syntax" into real errors.
+
+"use strict";
+x = {p1:10, p2:20};      // This will cause an error
+
+"use strict";
+let x = 3.14;
+delete x;                // This will cause an error
+
+"use strict";
+let x = 010;             // This will cause an error
+
+"use strict";
+eval ("let x = 2");
+alert (x);             // This will cause an error, eval is keyword
+
+"use strict";
+function myFunction() {
+  alert(this); // will alert "undefined"
+}
+myFunction();
+
+"use strict";
+with (Math){x = cos(2)}; // This will cause an error
+
+"use strict";
+delete Object.prototype; // This will cause an error
+
+"use strict";
+const obj = {get x() {return 0} };
+
+obj.x = 3.14;            // This will cause an error
+
+"use strict";
+const obj = {};
+Object.defineProperty(obj, "x", {value:0, writable:false});
+
+obj.x = 3.14;            // This will cause an error
