fefafbcbc36992be6495899ad08bbfdd50fb128a
Pranab Sarkar <86111675+devpranab@users.noreply.github.com>
2022-02-11 14:45:35 +0530

    Update 15-stringTemp.js

--- 15-stringTemp.js
+++ 15-stringTemp.js
@@ -1,5 +1,17 @@
 //String templates literals - es-6 - no need using concat
 
+// JavaScript Template Literals
+// Synonyms:
+// Template Literals
+//let text = `He's often called "Johnny"`;
+
+// Template Strings
+
+// String Templates
+
+// Back-Tics Syntax
+//let text = `Hello World!`;
+
 //before
 // console.log("First line \nSecond line");
 // console.log("Second line");
@@ -27,4 +39,20 @@ var a = 21
 var b = 23
 var result = `${a} + ${b} = ${a + b}`;
 console.log(result);
+*/
+
+/*
+/*
+HTML Templates
+Example
+let header = "Templates Literals";
+let tags = ["template literals", "javascript", "es6"];
+
+let html = `<h2>${header}</h2><ul>`;
+for (const x of tags) {
+  html += `<li>${x}</li>`;
+}
+
+html += `</ul>`;
+*/
 */
