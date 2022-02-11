926eb952b3690db165b8d354b88d82016fb4bb5d
Pranab Sarkar <86111675+devpranab@users.noreply.github.com>
2022-02-11 18:32:34 +0530

    Update 23-debugging.js
    debugger kw

--- 23-debugging.js
+++ 23-debugging.js
@@ -1,1 +1,9 @@
 //Debugging
+
+// const numbers = [175, 50, 25];
+
+// function myFunc(total, num) {
+//   return total - num; //return 100
+// }
+// let result = numbers.reduce(myFunc);
+// console.log(result);
