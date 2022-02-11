7de1c22204bba28a75d47b3e105a0b82435fa1b5
Pranab Sarkar <86111675+devpranab@users.noreply.github.com>
2022-02-11 18:54:07 +0530

    Add files via upload

---
+++ 30-typeofOp.js
@@ -0,0 +1,17 @@
+//typeof operator
+
+//some confused data,don't easy to understand
+
+// typeof "John"                 // Returns "string"
+// typeof 3.14                   // Returns "number"
+// typeof NaN                    // Returns "number"
+// typeof false                  // Returns "boolean"
+// typeof [1,2,3,4]              // Returns "object"
+// typeof {name:'John', age:34}  // Returns "object"
+// typeof new Date()             // Returns "object"
+// typeof function () {}         // Returns "function"
+// typeof myCar                  // Returns "undefined"
+// typeof null                   // Returns "object"
+
+//the constructor property returns the constructor function for all JavaScript variables
+//let x = "John".constructor  // Returns function String()  {[native code]}
