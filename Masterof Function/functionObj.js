735d23f9946971bc64d9ecadab74c9e65fc0b11f
Pranab Sarkar <86111675+devpranab@users.noreply.github.com>
2022-02-25 11:57:00 +0530

    Update functionObj.js
    2nd & 3rd prove that function print as a obj

--- Masterof Function/functionObj.js
+++ Masterof Function/functionObj.js
@@ -8,6 +8,32 @@
 // console.log(typeof test); //type is function
 
 //functions are object
-//console.log(test.constructor); //prove 1
+//console.log(test.constructor); //prove 1 - f function() {[native code]}
 
 //using constructor make a function
+// let Rect = new Function("width", "height", `this.width = width
+// this.height = height
+
+// this.draw = function(){
+// console.log("i am reactangle")
+// this.printProperties()
+// console.log(this)
+// }
+// this.printProperties = function(){
+// console.log("my width is" + this.width)
+// console.log("my height is" + this.height)
+// }`                       
+// ){ 
+// }       //function works in this way behind the scene
+
+// //call
+// let rect1 = new Rect(5,6);
+// console.log(react1); //output will come as a obj
+
+let rect1.width();
+
+
+function test(){
+console.log("random text");
+}
+console.log(test.name, test.length); //name, length is bult-in
